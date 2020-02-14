27b83b4197e31ce5b8c36f692fb7695e9fa79856
Gaurangsharma <gaurang.sharma_cs16@gla.ac.in>
2020-02-14 17:12:07 +0530

    redis get set

--- encription_redis.py
+++ encription_redis.py
@@ -1,22 +1,34 @@
+# import redis
+
+
+# try:
+#     conn=redis.StrictRedis(
+#     host='localhost',
+#     port=6379,
+#     password='',
+#     ssl=True,
+#     decode_responses=True
+# )   
+#     print (conn)
+#     print ('Connected!')
+#     print ('before set!')
+#     conn.set('nnn', 'yguyfgyuvf')
+#     print ('after set!')
+#     msg = conn.get('nnn')
+
+#     print(msg)
+# except Exception as ex:
+#     print ('Error:', ex)
+#     exit('Failed to connect, terminating.')
+
+
 import redis
 
-
 try:
-    conn=redis.StrictRedis(
-    host='localhost',
-    port=6379,
-    password='',
-    ssl=True,
-    decode_responses=True
-)   
-    print (conn)
-    print ('Connected!')
-    print ('before set!')
-    conn.set('nnn', 'yguyfgyuvf')
-    print ('after set!')
-    msg = conn.get('nnn')
-
-    print(msg)
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    print(r)
+    r.set('foo', 'bar')
+    print('value "foo" from Redis: {}'.format(r.get('foo')))
 except Exception as ex:
     print ('Error:', ex)
     exit('Failed to connect, terminating.')
